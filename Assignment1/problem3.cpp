9771c3b071510f4a2c4a9dfa525a3c3d2c8f8867
Alex <asutherland8219@gmail.com>
2023-05-18 07:55:47 -0300

    finished problem 3

--- Assignment1/problem3.cpp
+++ Assignment1/problem3.cpp
@@ -7,19 +7,40 @@
 
 using namespace std;
 
-int rowConversion(int, char);
+double rowConversion(int, char);
 void tableHeaders();
 
 void tempConversionTable() {
     // parent function 
     int increment = 5;
     int floor = -40;
+    int ceiling = 455;
 
     tableHeaders();
+
+    for ( floor; ceiling; floor += increment) {
+
+        // column one, F to C 
+        cout << "\t" << floor << "\t";
+        cout << rowConversion(floor, 'F');
+
+        // pipe
+        cout << "\t|";
+
+        // column two, C to F 
+        cout << "\t" << floor << "\t";
+        cout << rowConversion(floor, 'C') << "\n";
+
+
+        if ( floor == ceiling){
+            break;
+        }
+    }
+
 }
 
-int rowConversion(int temperature, char units) {
-    int calculation;
+double rowConversion(int temperature, char units) {
+    double calculation;
 
     if (units == 'C') {
         calculation = (temperature * 9 / 5) + 32 ;
@@ -32,6 +53,8 @@ int rowConversion(int temperature, char units) {
 }
 
 void tableHeaders() {
-    cout << "\t" << "Temperature \t" << "|" << "\t" << "Temperature \t \n";
-    cout << "\t" << "(degrees) \t" << "|" << "\t" << "(degrees) \t \n";
+    cout <<  setw(5) << "\t" << "Temperature \t" << "|" << "\t" << "Temperature \t \n";
+    cout << "\t" << " (degrees) \t" << "|" << "\t" << " (degrees) \t \n";
+    cout << "\t" << "F" << "\t" << setw(3) << "C";
+    cout << "\t|\t" << "C" << "\t" << setw(5) << "F \n";
 }
