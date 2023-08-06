c295c1227258e354d28523f4946e13ffa5806cd1
Alex <asutherland8219@gmail.com>
2023-08-06 18:53:02 -0300

    some syntax changes

--- Assignment2/problem3_Elevator.cpp
+++ Assignment2/problem3_Elevator.cpp
@@ -8,7 +8,7 @@
 
 class Elevator{
 private:
-    int position = 0;
+    int position = 1;
     int top = 0;
 public:
     Elevator(int N = 5){
@@ -19,30 +19,46 @@ public:
 
     void up(){
         // move up a floor
-        position++;
-        if (position >= top){
-            std::cout << "You have reached the top floor." << std::endl;
+        if (in_range(position)) {
+            position++;
+            std::cout << "Going up." << std::endl;
         }
-
     }
 
     void down(){
         //move down a floor
-        position--;
-        if (position <= 0){
-            std::cout << "You have reached the main floor." << std::endl;
+        if (in_range(position)) {
+            position--;
+            std::cout << "Going down." << std::endl;
         }
+
     }
 
     void go_to(int N){
         // go to a specific floor
-        position = N;
-        if (position >= top ){
-            std::cout << "This value is more than the maximum of floors in this building." << std::endl;
-            std::cout << "Please enter a value between " << 1 << "and " << top << std::endl;
-
+        if (in_range(position)) {
+            position = N;
+            std::cout << "We have arrived on floor " << position << std::endl;
         }
+
     }
+
+    bool in_range(int desired_floor){
+        // checks if the value is in the range, if it is do nothing. If it isnt print either too high or too low
+        if (desired_floor > top ){
+            std::cout << "This value is more than the maximum of floors in this building.\n" << std::endl;
+            return false;
+        }
+        else if (desired_floor < 1 ){
+            std::cout << "This is less than the number of floors in this building. \n" << std::endl;
+            std::cout << "Please enter a value between " << 1 << "and " << top << std::endl;
+            return false;
+        }
+        else {
+            return true;
+        }
+
+    };
 
 
 };
