50c6bff6d954f9adc687cbaeabb72979ee9fcb5a
Alex <asutherland8219@gmail.com>
2023-09-02 15:58:13 -0300

    add a menu

--- Assignment3/Problem1.cpp
+++ Assignment3/Problem1.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <filesystem>
 
 class File{
 public:
@@ -16,9 +17,48 @@ public:
         std::cout << "Please enter the name of the file you would like to open (make sure to include the extension): ";
         std::cin >> fileName;
         if (CheckType()) {
+            char selection;
+            std:: cout << "You have entered the following path or filename: " << fileName << std::endl;
+            std:: cin >> selection;
+            selectionMenu(selection);
+
+
+
+
+
         }
     };
 
+    void selectionMenu(char selection) {
+        // a menu to select what function you want to execute
+        bool running = true;
+        void userInput(char);
+
+        while (running) {
+            std:: cout << "What function would you like to execute in regards to this file? Please make a selection from the list below: " << std::endl;
+            std:: cout << "\t 1. Count\n";
+            std:: cout << "\t 2. Print Line By Line\n";
+            std:: cout << "\t 3. Text Reader \n";
+            std:: cout << "\t 4. Text Reader Demo\n";
+            std:: cout << "\t x. Exit\n";
+
+            std:: cout << "Selected: " << selection << std::endl;
+
+            if (selection == 'x' || selection == 'X') {
+                running = false;
+                std::cout << "\nThank you. Good bye. \n";
+                break;
+            } else {
+                userTextInput(selection);
+            }
+    }
+        }
+
+    void userTextInput(char selection) {
+        switch (selection) {
+
+        }
+    }
     void ReadFile() {
             // A function that will be used to read the file and then print the new output
         };
@@ -28,12 +68,16 @@ public:
     void ReadWriteFile() {
             // A function that will be used to read the file, print the output, then write the file and print the new modified output
         };
+    void Count() {
+            // A function that counts the number of words in a file
+
+    }
 
 
     bool CheckType() {
         // we only want text files so lets check to make sure the suffix of the file path or the file name
-
-        if (fileName.rfind(".") == fileName.size() - 1 != true) {
+        std::filesystem::path temp_path = fileName;
+        if (temp_path.extension() != ".txt") {
             std::cout << "Whoops, wrong file format. Please enter a supported text file (.txt). " << std::endl;
             return false;
         };
