b18dc0f7ae4f81da5ae6dae2f4f8e5757a109cc6
Asutherland8219 <60415557+Asutherland8219@users.noreply.github.com>
2023-10-09 15:26:49 -0300

    a third or more done assignment 3

--- Assignment3/Assignment3Question1.cpp
+++ Assignment3/Assignment3Question1.cpp
@@ -10,7 +10,7 @@ using namespace std;
 class File{
 public:
     // A function that will be used to read in the file text and output it
-    std::ifstream txtfile; // create the stream object
+
 
     // let the user input the path of the file they want to open
     std::string fileName;
@@ -18,18 +18,30 @@ public:
         std::cout << "Please enter the name of the file you would like to open (make sure to include the extension): ";
         std::cin >> fileName;
         if (CheckType()) {
-            selectionMenu();
+            // append to the path
+            std::filesystem::path filePath = std::filesystem::current_path() /"Assignment3" / fileName;
 
+            // Check if the file exists
+            if (!std::filesystem::exists(filePath)) {
+                std::cout << "Error: The file '" << fileName << "' does not exist." << std::endl;
+                return;
+            }
 
+            std::ifstream inputFile(filePath); // create the stream object
 
+            if (!inputFile.is_open()) {
+                std::cout << "Error. Could not open the file entered." << endl;
 
+            }
 
-
-
+            else {
+                std::cout << "File opened." << endl;
+                selectionMenu(inputFile);
+            }
         }
     };
 
-    void selectionMenu() {
+    void selectionMenu(std::ifstream& inputFile) {
         // a menu to select what function you want to execute
         bool running = true;
 
@@ -51,49 +63,70 @@ public:
                 std::cout << "\nThank you. Good bye. \n";
                 break;
             } else {
-                userTextInput(selection);
+                userTextInput(selection, inputFile);
             }
     }
         }
 
-    void userTextInput(char selection) {
+    void userTextInput(char selection, std::ifstream& inputFile) {
         switch (selection) {
             case '1':
                 // Count
-                Count();
+                Count(inputFile);
                 break;
             case '2':
                 // Print Line By Line
-                PrintLineByLine();
+                PrintLineByLine(inputFile);
                 break;
             case '3':
                 // Text reader
-                ReadFile();
+                ReadFile(inputFile);
                 break;
             case '4':
                 // Text reader demo
-                ReadFileDemo();
+                ReadFileDemo(inputFile);
                 break;
             default:
                 std::cout << "Invalid selection. Please select an item from the options listed. \n";
         }
     }
-    void ReadFile() {
+    void ReadFile(std::ifstream& inputFile) {
             // A function that will be used to read the file and then print the new output
         };
-    void ReadFileDemo() {
+    void ReadFileDemo(std::ifstream& inputFile) {
         // A function that will be used to write the file and then print the new output
     };
-    void WriteFile() {
-            // A function that will be used to write the file and then print the new output
-        };
-    void Count() {
-            // A function that counts the number of words in a file
+    void Count(std::ifstream& inputFile) {
+        // A function that counts the number of words in a file
+        std::string word;
+        int num_word = 0;
+        while (inputFile >> word) {
+            num_word++;
+        }
+        std::cout << "Word count: " << num_word << std::endl;
 
     };
-    void PrintLineByLine() {
+    void PrintLineByLine(std::ifstream& inputFile) {
+        std::string txt_line;
 
-    };
+        while (std::getline(inputFile, txt_line)) {
+            // print the line
+            char userInput;
+            std::cin.get(userInput);
+
+            if (userInput == '\n') {
+                std::cout << txt_line << std::endl;
+            }
+            else if (userInput == 'x' || userInput == 'X') {
+                return; // Exit the function
+            }
+            else {
+                std::cout << "Please press enter to continue or enter x to exit." << std::endl;
+                std::cin.ignore();
+            }
+        }
+    }
+
 
 
     bool CheckType() {
