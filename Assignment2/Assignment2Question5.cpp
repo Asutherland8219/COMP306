fcd0bdc0929021d6d2946bbbcc7a883c0e364767
Asutherland8219 <60415557+Asutherland8219@users.noreply.github.com>
2023-11-13 20:18:50 -0400

    started circle caclulations for bounding box

--- Assignment2/Assignment2Question5.cpp
+++ Assignment2/Assignment2Question5.cpp
@@ -3,31 +3,46 @@
 //
 
 #include <iostream>
+#include <vector>
 
 class Point {
 private:
-    double x;
-    double y;
+    double x = 0;
+    double y = 0;
 public:
+    Point() : x(0) , y(0)  {
+            std::cout << "You have not entered any values so the default point object is created at 0,0. \n";
+        };
+
+    Point(double x, double y) : x(x), y(y) {
+
+    }
+    double getX() const {
+        return x;
+    }
+    double getY() const {
+        return y;
+    }
+
    void Show() {
-       std::cout << "x and y are currently set at: (" << x << "," << y << ")";
+       std::cout << "x and y are currently set at: (" << x << "," << y << ") \n";
     }
 
     void Add( int x_change = 0, int y_change = 0) {
         // have both variables and set default to 0, because they have a default value, we can treat them as optional
         Show();
 
-        if (x_change != 0) {
+        if (y_change != 0) {
             // this means that we only want to change the y value
             y = y + y_change;
         }
 
-        if (y_change != 0) {
+        if (x_change != 0) {
             // this means we only want to change the x value
             x = x + x_change;
         }
 
-        std::cout << "The new point values for x and y are set at: " << x << " , " << y ;
+        std::cout << "The new point values for x and y are set at: " << x << " , " << y << endl;
 
     }
 
@@ -35,20 +50,21 @@ public:
         // have both variables and set default to 0, because they have a default value, we can treat them as optional
         Show();
 
-        if (x_change != 0) {
+        if (y_change != 0) {
             // this means that we only want to change the y value
             y = y - y_change;
         }
 
-        if (y_change != 0) {
+        if (x_change != 0) {
             // this means we only want to change the x value
             x = x - x_change;
         }
 
-        std::cout << "The new point values for x and y are set at: " << x << " , " << y ;
+        std::cout << "The new point values for x and y are set at: " << x << " , " << y << endl;
 
     }
 };
+
 
 class Shape {
 public:
@@ -56,16 +72,26 @@ public:
         // default constructor
     }
 
-    virtual void boundingBox() = 0;
+    virtual void display() {
+        std::cout << "Area: " << area() << endl;
+        std::cout << "Circumference: " << circumference() << endl;
+//        std::cout << "Bounding Box: " << boundingBox() << endl;
+    };
 
-    virtual double circumference() = 0;
+    virtual std::vector<Point> boundingBox() {
+        return {};
 
-    virtual double area() = 0;
+    };
 
-    virtual void display();
+    virtual double circumference() {
 
+    };
 
+    virtual double area() {
+
+    };
 };
+
 
 class Circle : public Shape {;
 private:
@@ -73,7 +99,7 @@ private:
     double radius;
     // circumference
 public:
-    Circle(Point center, double radius) {
+    Circle(Point center, double radius) : center(center), radius(radius) {
         // check that the value is positive
         if (radius <= 0) {
             cout << "Value is not possible for a circle. Please enter a number greater than 0. \n";
@@ -81,15 +107,24 @@ public:
     }
 
     double area() override  {
-
+        double circle_area = 3.14159 * radius * radius;
+        return circle_area;
     };
 
     double circumference() override  {
-
+        double circle_circumference = 2 * 3.14159 * radius;
+        return circle_circumference;
     };
 
-    void boundingBox() override
+    std::vector<Point> boundingBox() override
     {
+        // Calculate the bounding box variables
+        double left_x = center.getX() - radius;
+        double left_y = center.getY() - radius;
+        double right_x = center.getX() + radius;
+        double right_y = center.getY() + radius;
+
+        // Create the multiple points  of the surrounding box
     }
 
 };
@@ -104,7 +139,7 @@ class Rectangle : public Shape {
 
     };
 
-    void boundingBox() override
+    std::vector<Point> boundingBox() override
     {
     }
 };
@@ -119,7 +154,7 @@ class Triangle : public Shape {
 
     };
 
-    void boundingBox() override
+    std::vector<Point> boundingBox() override
     {
     }
 };
--- Assignment2/main.cpp
+++ Assignment2/main.cpp
@@ -9,149 +9,180 @@ using namespace std;
 
  int main() {
 
-    // PROBLEM 1 -- > Done
-    string input;
+//    // PROBLEM 1 -- > Done
+//    string input;
+//
+//    Animal animal;
+//    // default constructor
+//    animal.sound();
+//
+//    // Pig
+//    Pig pig;
+//    pig.sound();
+//
+//    // Sheep
+//    Sheep sheep;
+//    sheep.sound();
+//
+//    // Duck
+//    Duck duck;
+//    duck.sound();
+//
+//    // Cow
+//    Cow cow;
+//    cow.sound();
+//
+//
+//    // Test input function --> Problem 1
+//    cout << "Enter an animal: ";
+//    cin >> input;
+//
+//    while (!AnimalTest::Test(input)) {
+//         cout << "Enter an animal: ";
+//         cin >> input;
+//    }
+//
+//    // PROBLEM 2 --> Done
+//    // Test function for the Book class ; mainly on creation
+//    // test getters in the main
+//    Book red_rising = Book();
+//
+//    red_rising.setTitle("Red Rising");
+//    red_rising.setISBN("034553980X");
+//    red_rising.setAuthor("Pierce Brown");
+//    red_rising.setEdition("1st Edition");
+//    red_rising.setPublisher("Del Rey Books");
+//    red_rising.setYearPublished("Jan. 1 2014");
+//    red_rising.details();
+//
+//    // create a second book, this time only getting one item at a time
+//    Book golden_son = Book();
+//    golden_son.setTitle("Golden Son");
+//    golden_son.setISBN("0345539818");
+//    golden_son.setAuthor("Pierce Brown");
+//    golden_son.setEdition("1st Edition");
+//    golden_son.setPublisher("Del Rey Books");
+//    golden_son.setYearPublished("Jan. 6 2015");
+//
+//    // get all the details
+//    cout << golden_son.get("title") << "\n";
+//    cout << golden_son.get("isbn") << "\n";
+//    cout << golden_son.get("author") << "\n";
+//    cout << golden_son.get("edition") << "\n";
+//    cout << golden_son.get("publisher")<< "\n";
+//    cout << golden_son.get("year published") << "\n";
+//
+//    // PROBLEM 3 ; The elevator --> Done
+//
+//    // init the elevator
+//    Elevator elevator = Elevator();
+//
+//    // go all the way up to floor 5, one at a time
+//    elevator.up();
+//    elevator.up();
+//    elevator.up();
+//    elevator.up();
+//
+//    // test the max floor error response
+//    elevator.up();
+//
+//    // go all the way down
+//    elevator.down();
+//    elevator.down();
+//    elevator.down();
+//    elevator.down();
+//
+//    // go beyond first floor to test error response
+//    elevator.down();
+//
+//    // reset to one (main floor)
+//    elevator.reset();
+//
+//    // go to floor outside upper limit
+//    elevator.go_to(6);
+//
+//    // go to floor outside lower limit
+//    elevator.go_to(-1);
+//
+//    // go to specific floor, then reset
+//    elevator.go_to(3);
+//    elevator.reset();
+//
+//    // create a big elevator, for a tower with 20 floors
+//    Elevator big_elevator = Elevator(20);
+//    big_elevator.go_to(20);
+//
+//    // PROBLEM 4 --> Done
+//    Mouse mouse("Lola");
+//    mouse.jump();
+//    mouse.dance();
+//    mouse.eat("cheese");
+//    mouse.move();
+//    mouse.sleep();
+//    mouse.groom();
+//    // unique ability
+//    mouse.hide();
+//
+//    Gerbil gerbil("Gibby");
+//    gerbil.jump();
+//    gerbil.dance();
+//    gerbil.eat("apple");
+//    gerbil.move();
+//    gerbil.sleep();
+//    gerbil.groom();
+//    // unique
+//    gerbil.smell();
+//
+//    Hamster hamster("Hammy");
+//    hamster.jump();
+//    hamster.dance();
+//    hamster.eat("carrot");
+//    hamster.move();
+//    hamster.sleep();
+//    hamster.groom();
+//    // unique
+//    hamster.exercise();
+//
+//    GuineaPig guineapig("Gerald-Paul");
+//    guineapig.jump();
+//    guineapig.dance();
+//    guineapig.eat("celery");
+//    guineapig.move();
+//    guineapig.sleep();
+//    guineapig.groom();
+//    //unique
+//    guineapig.talk();
 
-    Animal animal;
-    // default constructor
-    animal.sound();
+    // PROBLEM 5
 
-    // Pig
-    Pig pig;
-    pig.sound();
+    // Init a base point function
+    Point base;
+    base.Show();
 
-    // Sheep
-    Sheep sheep;
-    sheep.sound();
+    // Increase x and y
+    base.Add(1, 1);
 
-    // Duck
-    Duck duck;
-    duck.sound();
+    // Increase x
+    base.Add(1);
 
-    // Cow
-    Cow cow;
-    cow.sound();
+    // Increase y
+    base.Add(0,1);
 
+    // Decrease x and y
+    base.Subtract(1, 1);
 
-    // Test input function --> Problem 1
-    cout << "Enter an animal: ";
-    cin >> input;
+    // Decrease x
+    base.Subtract(1);
 
-    while (!AnimalTest::Test(input)) {
-         cout << "Enter an animal: ";
-         cin >> input;
-    }
+    // Decrease y
+    base.Subtract(0, 1);
 
-    // PROBLEM 2 --> Done
-    // Test function for the Book class ; mainly on creation
-    // test getters in the main
-    Book red_rising = Book();
+    // Create a shape (circle)
+    Point circle_point(1,1);
+    Circle base_circle(circle_point, 10);
 
-    red_rising.setTitle("Red Rising");
-    red_rising.setISBN("034553980X");
-    red_rising.setAuthor("Pierce Brown");
-    red_rising.setEdition("1st Edition");
-    red_rising.setPublisher("Del Rey Books");
-    red_rising.setYearPublished("Jan. 1 2014");
-    red_rising.details();
-
-    // create a second book, this time only getting one item at a time
-    Book golden_son = Book();
-    golden_son.setTitle("Golden Son");
-    golden_son.setISBN("0345539818");
-    golden_son.setAuthor("Pierce Brown");
-    golden_son.setEdition("1st Edition");
-    golden_son.setPublisher("Del Rey Books");
-    golden_son.setYearPublished("Jan. 6 2015");
-
-    // get all the details
-    cout << golden_son.get("title") << "\n";
-    cout << golden_son.get("isbn") << "\n";
-    cout << golden_son.get("author") << "\n";
-    cout << golden_son.get("edition") << "\n";
-    cout << golden_son.get("publisher")<< "\n";
-    cout << golden_son.get("year published") << "\n";
-
-    // PROBLEM 3 ; The elevator --> Done
-
-    // init the elevator
-    Elevator elevator = Elevator();
-
-    // go all the way up to floor 5, one at a time
-    elevator.up();
-    elevator.up();
-    elevator.up();
-    elevator.up();
-
-    // test the max floor error response
-    elevator.up();
-
-    // go all the way down
-    elevator.down();
-    elevator.down();
-    elevator.down();
-    elevator.down();
-
-    // go beyond first floor to test error response
-    elevator.down();
-
-    // reset to one (main floor)
-    elevator.reset();
-
-    // go to floor outside upper limit
-    elevator.go_to(6);
-
-    // go to floor outside lower limit
-    elevator.go_to(-1);
-
-    // go to specific floor, then reset
-    elevator.go_to(3);
-    elevator.reset();
-
-    // create a big elevator, for a tower with 20 floors
-    Elevator big_elevator = Elevator(20);
-    big_elevator.go_to(20);
-
-    // PROBLEM 4 --> Done
-    Mouse mouse("Lola");
-    mouse.jump();
-    mouse.dance();
-    mouse.eat("cheese");
-    mouse.move();
-    mouse.sleep();
-    mouse.groom();
-    // unique ability
-    mouse.hide();
-
-    Gerbil gerbil("Gibby");
-    gerbil.jump();
-    gerbil.dance();
-    gerbil.eat("apple");
-    gerbil.move();
-    gerbil.sleep();
-    gerbil.groom();
-    // unique
-    gerbil.smell();
-
-    Hamster hamster("Hammy");
-    hamster.jump();
-    hamster.dance();
-    hamster.eat("carrot");
-    hamster.move();
-    hamster.sleep();
-    hamster.groom();
-    // unique
-    hamster.exercise();
-
-    GuineaPig guineapig("Gerald-Paul");
-    guineapig.jump();
-    guineapig.dance();
-    guineapig.eat("celery");
-    guineapig.move();
-    guineapig.sleep();
-    guineapig.groom();
-    //unique
-    guineapig.talk();
+    // Display
+    base_circle.display();
 
      return 0;
 
