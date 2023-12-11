9327f7f85ee5120f8d5e38f80ae01ec3ca8471df
Asutherland8219 <60415557+Asutherland8219@users.noreply.github.com>
2023-12-11 18:48:20 -0400

    almost finished problem 5

--- Assignment3/Assignment3Question5.cpp
+++ Assignment3/Assignment3Question5.cpp
@@ -6,6 +6,7 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <map>
 
 using namespace std;
 
@@ -19,9 +20,26 @@ private:
     string year_published;
 
 public:
-    //Constructor
-    Book(string title_str, string isbn_str, string author_str, string edition_str, string publisher_str, string year_published) :
-    title(title_str), ISBN(isbn_str), author(author_str), edition(edition_str), publisher(publisher_str), year_published(year_published) {}
+    // Constructor that takes an array of strings for book information
+    Book(const std::vector<std::string>& bookInfo) {
+        if (bookInfo.size() == 6) {
+            title = bookInfo[0];
+            ISBN = bookInfo[1];
+            author = bookInfo[2];
+            edition = bookInfo[3];
+            publisher = bookInfo[4];
+            year_published = bookInfo[5];
+        }
+        else {
+            throw std::invalid_argument("You are missing some mandatory fields to create a book. Please ensure you have the following details: \n"
+                                        "title \n"
+                                        "isbn \n"
+                                        "author \n"
+                                        "edition \n"
+                                        "publisher \n"
+                                        "year_published \n");
+        };
+    }
 
     //get
     string get(string query) {
@@ -85,48 +103,136 @@ public:
 
 class Bookshelf {
 public:
-    static void sortBooks(std::vector<Book>& books) {
-        std::sort(books.begin(), books.end(), BookComparator());
-    }
+    static std::vector<Book> books;
 
-    static void displayBooks(const std::vector<Book>& books, const std::string& message) {
-        std::cout << message << std::endl;
-        for (const auto book : books) {
-
+    static void displayBooks(std::vector<Book> &finished_books, const std::string &message, const std::string field = "title") {
+        if (field == "title") {
+            std::cout << message << std::endl;
+        }
+        else {
+            std::cout << message << field << std::endl;
+        }
+        for (auto book: finished_books) {
             book.details();
+            std::cout << std::endl;
         }
         std::cout << std::endl;
     }
 
-    static void main() {
+    static void testBookshelf() {
         // Create a dozen books
-        std::vector<Book> books = {
-                {"Book1", "123456", "Author1", "First Edition", "Publisher1", 2000},
-                {"Book2", "789012", "Author2", "Second Edition", "Publisher2", 1998},
-
+        std::vector<std::vector<std::string>> books_raw = {
+                {"Red Rising",  "034553980X", "Pierce Brown", "First Edition",  "Del Rey", "2014"},
+                {"Golden Son", " 0345539834 ", "Pierce Brown", "Second Edition", "Del Rey", "2015"},
+                {"Morning Star",  "9781444759075", "Pierce Brown", "First Edition",  "Del Rey", "2017"},
+                {"Iron Gold", " 147364657X ", "Pierce Brown", "Second Edition", "Del Rey", "2018"},
+                {"Dark Age",  "0425285960", "Pierce Brown", "First Edition",  "Del Rey", "2020"},
+                {"Lightbringer", "0425285979", "Pierce Brown", "Second Edition", "Del Rey", "2023"},
+                {"East of Eden",  "9780140186390", "John Steinbeck", "First Edition",  "Viking Press", "1952"},
+                {"Of Mice and Men", "78901266754", "John Steinbeck", "Second Edition", "Covici Friede", "1937"},
+                {"Babel",  "9780063021426", "R.F. Kuang", "First Edition",  "Harper Voyager", "2020"},
+                {"The Name of the Wind", "756404079", "Patrick Rothfuss", "Second Edition", "DAW Books", "2007"},
+                {"A Wise Man's Fear",  "9780756404734", "Patrick Rothfuss", "First Edition",  "DAW Books", "2011"},
+                {"The Art of War", "000000000001", "Sun Tzu", "Archaic Edition", "Sun Tzu Publishing", "400"},
         };
 
-        // Display books in the order they were entered
-        displayBooks(books, "Books in the order they were entered:");
+        bulkCreateBooks(books_raw);
 
-        // Sort books and display again
-        sortBooks(books);
-        displayBooks(books, "Books sorted by title and year of publication:");
+        // Display books in the order they were entered
+        displayBooks(books, "Books in the order they were entered: ");
+
+        // Sort books by year published
+        string field = "YEAR PUBLISHED";
+        bookSort(books, field);
+        displayBooks(books, "Books sorted by entered field: ", field);
+
+
+        // display books by sorting default of Title
+        bookSort(books);
+        displayBooks(books, "Books sorted by Default Field:" );
+        books.clear();
+    }
+
+    static void testInvalidBook() {
+        std::vector<std::vector<std::string>> books_raw_incorrect = {
+                {"Red Rising",  "034553980X", "Pierce Brown", "First Edition",  "Del Rey"},
+        };
+        bulkCreateBooks(books_raw_incorrect);
+        books.clear();
+    }
+
+    static void testSecondCondition() {
+        // Test that if the name is the same, the books are ordered by year published
+        std::vector<std::vector<std::string>> books_raw_same = {
+                {"Red Rising",  "034553980X", "Pierce Brown", "First Edition",  "Del Rey", "2015"},
+                {"Red Rising", " 0345539834 ", "Pierce Brown", "Second Edition", "Del Rey", "2000"},
+                {"Red Rising", " 0345539833 ", "Pierce Brown", "Second Edition", "Del Rey", "2023"},
+        };
+        bulkCreateBooks(books_raw_same);
+        bookSort(books);
+        displayBooks(books, "Books with the same title sorted by year published: ");
+        books.clear();
+    }
+
+    static void testDuplicateEntries() {
+        std::vector<std::vector<std::string>> books_raw_same = {
+                {"Red Rising",  "034553980X", "Pierce Brown", "First Edition",  "Del Rey", "2015"},
+                {"Red Rising", " 0345539834 ", "Pierce Brown", "Second Edition", "Del Rey", "2000"},
+                {"Red Rising", " 0345539834 ", "Pierce Brown", "Second Edition", "Del Rey", "2023"},
+        };
+        bulkCreateBooks(books_raw_same);
+    }
+
+    static bool compareBooks(Book book1, Book book2, std::string field = "TITLE") {
+        std::string value1 = book1.get(field);
+        std::string value2 = book2.get(field);
+
+        // Because the year value is a converted int, sorting is difficult.
+        // We change it back to int before sorting
+        if (field == "TITLE" && value1 == value2) {
+            field = "YEAR PUBLISHED";
+            value1 = book1.get(field);
+            value2 = book2.get(field);
+        }
+
+        // Duplicate condition; isbn are unique
+        std::string isin1 = book1.get("isbn");
+        std::string isin2 = book2.get("isbn");
+        if (isin1 == isin2) {
+            throw invalid_argument("Duplicate entry: ISBN values are unique, please make sure you do not have duplicate entries.\n");
+        }
+
+        if (field == "YEAR PUBLISHED") {
+            return std::stoi(value1) < std::stoi(value2);
+        } else {
+            return value1 < value2;
+        }
+
+    }
+
+
+    static void bulkCreateBooks(std::vector<std::vector<std::string>>& books_raw) {
+        for (const auto &bookInfo: books_raw) {
+            try {
+                books.emplace_back(bookInfo);
+            } catch (const std::invalid_argument& e) {
+                std::cerr << "Error creating book: " << e.what() << std::endl;
+            }
+        }
+    };
+
+    // Function to sort books based on user-specified criteria
+    static void bookSort(std::vector<Book>& books, const std::string& sortCriteria = "TITLE") {
+        std::sort(books.begin(), books.end(), [&sortCriteria](const Book& book1, const Book& book2) {
+            return compareBooks(book1, book2, sortCriteria);
+        });
     }
 };
 
-    void bookSort(std::vector<Book> books, string field = "title" )
-    // default to title if no explicit field is given
 
-    {
 
-};
+std::vector<Book> Bookshelf::books;
 
-// function for checking for duplicates
-
-    bool bookCheck() {
-
-};
 
 
 
--- Assignment3/main.cpp
+++ Assignment3/main.cpp
@@ -3,15 +3,34 @@
 //
 #include "inputMenu.cpp"
 #include "Assignment3Question4.cpp"
-
+#include "Assignment3Question5.cpp"
 int main() {
     // Question 1, 2, 3
-    inputMenu();
+//    inputMenu();
 
     // Question 4
     floatingPointArray();
 
     // Question 5
+    Bookshelf bookshelf;
+    bookshelf.testBookshelf();
+
+    // Test second condition
+    bookshelf.testSecondCondition();
+
+    // Test duplicate entries
+    bookshelf.testDuplicateEntries();
+
+    // Test invalid book
+    bookshelf.testInvalidBook();
+
+
+
+
+
+
+
 
 
 };
+
