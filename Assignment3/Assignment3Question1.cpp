2667450a9bcc10ca3dea0911db0890101c398adb
Asutherland8219 <60415557+Asutherland8219@users.noreply.github.com>
2024-01-28 22:47:39 -0400

    finished assignment 1 plan, need to finish inputmenu

--- Assignment3/Assignment3Question1.cpp
+++ Assignment3/Assignment3Question1.cpp
@@ -1,6 +1,54 @@
-//
-// Created by alex on 27/08/23.
-//
+/*
+ Title: Assignment3Question1.cpp
+ Description: Counter number of words in a user input file. Must be txt.
+ Date: January 28th, 2024
+ Author: Alex Sutherland
+ StudentID: 3640392
+ Version: 1.0
+*/
+
+
+/*
+ DOCUMENTATION
+
+ Program Purpose:
+	Count number of words in a user entered file.
+
+ Compile (assuming Cygwin is running): g++ -o Assignment3 Assignment3Question1.cpp
+ Execution (assuming Cygwin is running): ./Assignment3Question1.exe
+
+ Classes (functions):
+	- No actual classes, just one function
+        - wordCount(inputFile)
+            - accepts an ifstream inputfile
+
+ Variables:
+ 	- string word : The word index
+ 	- int num_word : The word counter, incremented by each word counted
+ 	- std::ifstream inputFile : The ifstream object that is read in and counted
+
+*/
+
+/*
+ TEST PLAN
+
+ Normal case:
+    ifstream inputFile = "excerpt.txt"
+ 	> wordCount(inputFile)
+ 	    Output: "Word Count: xxx"
+
+
+ Bad Case
+    Because this is a user input, there are a lot of potential issues for bad cases. However, this is handled externally, before this function is hit.
+ 	> Bad input will not reach the function.
+
+
+ Discussion:
+ 	Program that counts words in an entered file. Must be a .txt file. No error handling as the function is called in a separate inputMenu function.
+ 	Program will not be run unless makes it through necessary checks.
+
+*/
+
 #include <iostream>
 #include <fstream>
 using namespace std;
--- Assignment3/inputMenu.cpp
+++ Assignment3/inputMenu.cpp
@@ -1,7 +1,63 @@
-//
-// Created by asuth on 11/1/2023.
-//
-#include <utility>
+/*
+ Title: inputMenu.cpp
+ Description: Input menu. Used to handle logic for Questions 1 through 3.
+ Date: January 28th, 2024
+ Author: Alex Sutherland
+ StudentID: 3640392
+ Version: 1.0
+*/
+
+
+/*
+ DOCUMENTATION
+
+ Program Purpose:
+	Cleaner and better handling of user inputs in Questions 1 through 3
+
+ Compile (assuming Cygwin is running): g++ -o Assignment3 inputMenu.cpp
+ Execution (assuming Cygwin is running): ./inputMenu.exe
+
+ Classes (functions):
+	- Class inputMenu()
+
+    - Functions:
+        - setPath(path)
+            - Sets the main path for where the file is located
+        - getPath()
+            - Getter for the path
+        - selectionMenu()
+            - The main menu function, outputs what the options are and reads the user input.
+            - Accepts 1 through 3 and the x input to exit
+        - userTextInput(selection)
+            - Takes the users input value, feeds into a switch statement that runs the matching function
+
+ Variables:
+ 	- string word : The word index
+ 	- int num_word : The word counter, incremented by each word counted
+ 	- std::ifstream inputFile : The ifstream object that is read in and counted
+
+*/
+
+/*
+ TEST PLAN
+
+ Normal case:
+    ifstream inputFile = "excerpt.txt"
+ 	> wordCount(inputFile)
+ 	    Output: "Word Count: xxx"
+
+
+ Bad Case
+    Because this is a user input, there are a lot of potential issues for bad cases. However, this is handled externally, before this function is hit.
+ 	> Bad input will not reach the function.
+
+
+ Discussion:
+ 	Program that counts words in an entered file. Must be a .txt file. No error handling as the function is called in a separate inputMenu function.
+ 	Program will not be run unless makes it through necessary checks.
+
+*/
+
 
 #include "Assignment3Question1.cpp"
 #include "Assignment3Question2.cpp"
