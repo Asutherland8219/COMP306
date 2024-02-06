dfc850b21ce4a47de01a75154b6ec27e16c66260
Asutherland8219 <60415557+Asutherland8219@users.noreply.github.com>
2024-02-05 22:14:39 -0400

    start chapter one(2)

--- Assignment4/Gates/Chapter_1/ChapterOne.cpp
+++ Assignment4/Gates/Chapter_1/ChapterOne.cpp
@@ -4,9 +4,33 @@
 
 #include <iostream>
 
-class ChapterOne {
-private:
+class ChapterOneGates {
 public:
+    bool panic_breaker;
+    bool panicChoice(int choice) {
+        switch (choice) {
+            case 1:
+                std::cout << "You vault over the edge and jump without a fear or care in the world!";
+                panic_breaker = true;
+                break;
+            case 2:
+                std::cout << "You reach into your pocket, and find a small bronze coin, you throw it in to the well...\n";
+                std::cout << "and you wait...\n";
+                std::cout << "and you wait...\n";
+                std::cout << "and you wait...\n";
+                std::cout << "But you never hear it hit the bottom.";
+                break;
+            case 3:
+                std::cout << "You turn around to walk away thinking nothing of what just happened. Just as you start to walk away, a bird swoops down in front of your face. \n"
+                             "Out of panic, you take a step back towards the well, losing your footing in the process and tumbling backwards while yelling: \n";
+                std::cout << "ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh...\n";
+                panic_breaker = true;
+                break;
+        }
+        return panic_breaker;
+    }
+
+
 
 
 };
--- Assignment4/Sections/Checkpoint1.cpp
+++ Assignment4/Sections/Checkpoint1.cpp
@@ -1,3 +1,47 @@
 //
 // Created by asuth on 12/13/2023.
 //
+#include <iostream>
+#include <string>
+#include "../Character/Character.h"
+#include "../NPC/NPC.h"
+#include "../Gates/Intro/intro_choices.cpp"
+#include "../Gates/Chapter_1/ChapterOne.cpp"
+
+class Checkpoint1 {
+private:
+    bool panic_breaker;
+
+public:
+    void Chapter1(Character custom_character) {
+        std::cout << "You fell engorged, almost like you are blowing up. You look down and see the world around you changing... \n";
+        std::cout << "But wait.. no that's not the case, the world isn't changing.. YOU are changing. You are growing! \n";
+        std::cout << "You look around at your hands and feet noticing them both grow in size. Before you even start to think about panicking...\n";
+        std::cout << "THUNK. You clunk your head on the ceiling...\n";
+
+        while (!Checkpoint1::panic_breaker){
+            std::cout << std::endl;
+            std::cout << "What would you like to do?\n";
+            std::cout << "1. Grab the key, and head for the door.\n";
+            std::cout << "2. PANIC!!!\n";
+            std::cout << "3. Look around for anything useful...\n";
+            int panic;
+            std::cin >> panic;
+
+            if (std::cin.fail()) {
+                throw std::invalid_argument("Invalid input. Please enter a number.");
+            }
+            else {
+                panic_breaker = ChapterOneGates::panicChoice(panic);
+            }
+        }
+
+
+
+
+
+
+
+
+    }
+};
