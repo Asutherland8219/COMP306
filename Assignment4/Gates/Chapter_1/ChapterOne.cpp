201885353a14111b664420dea60de171f0b15fee
Alex Sutherland <alexsutherland@Alexs-MacBook-Pro.local>
2024-02-15 20:50:59 -0400

    add some more choices context

--- Assignment4/Gates/Chapter_1/ChapterOne.cpp
+++ Assignment4/Gates/Chapter_1/ChapterOne.cpp
@@ -73,7 +73,7 @@ public:
         switch (rabbit_choice) {
             case 1:
                 std::cout << "You watch as the rabbit goes by, too nervous to say anything...\n"
-                             "You're head is killing you. You decide to lay down and close your eyes...";
+                             "You're head is killing you. You decide to lay down and close your eyes...\n \n";
 
                 std::cout << "Narrator: The character waits and waits but no one else ever comes by. Eventually, thirst and "
                              "starvation takes over. After a week, the character has moved on. \n"
@@ -82,31 +82,10 @@ public:
                 rabbit_breaker = false;
                 break;
             case 2:
-                std::cout << "You start to get dizzy. Thinking to yourself `Is this what a panic attack feels like`.\n";
-                std::cout << "Because of the dizzyness, you decide to sit down. \n";
-                std::cout << "On the way down you lose your balance and drop to the floor...\n";
-                std::cout << "THUNK! \n The whole building shakes when you sit on the floor.\n";
-                std::cout << "Slowly, you regain your orientation and take a few deep breaths...";
+                std::cout << "The rabbit slows down and turns to face you, tilting it's head with an inquisitive look \n";
                 break;
             case 3:
-                std::cout << "Looking around again, trying to find something useful you catch a glimpse of the bottle from earlier. \n";
-                std::cout << "You grab it and tip it up into your mouth... unfortunately nothing comes out \n";
-                std::cout << "A thought pops into your head, perhaps this bottle could be useful later... \n";
-                while (!bottle) {
-                    std::cout << "What do you want to do with the bottle? \n";
-                    std::cout << "1. Add the bottle to your inventory. \n";
-                    std::cout << "2. Discard the bottle. \n";
-
-                    auto input_bottle = getUserInput(custom_character);
-                    std::istringstream iss(input_bottle);
-
-                    int bottle_choice;
-                    if (iss >> bottle_choice) {
-                        ChapterOneGates::bottle = inventoryBottleChoice(bottle_choice, custom_character);
-                        panicChoice(1, custom_character);
-                    }
-
-                }
+                std::cout << "The rabbit jumps and lets out a yelp, indicating that you have startled the creature \n";
                 break;
         }
         return rabbit_breaker;
