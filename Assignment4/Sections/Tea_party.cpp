8a0a2215c913ec74c772c49670909cc99530897e
Asutherland8219 <60415557+Asutherland8219@users.noreply.github.com>
2024-02-23 20:56:54 -0400

    unno

--- Assignment4/Sections/Tea_party.cpp
+++ Assignment4/Sections/Tea_party.cpp
@@ -220,7 +220,88 @@ public:
 
         rabbit.talk("Oh, hush! ; The queen will hear you! You see, she came rather late, and the queen said—");
 
+        queen.talk("Get to your places!");
+        std::cout << "people began running about in all directions, tumbling up against each other; however, they got settled down in a minute or two, and the game began. \n";
 
+        std::cout << "You've never seen such a curious croquet-ground in her life; it was all ridges and furrows; \n "
+                     "the balls were live hedgehogs, the mallets live flamingos, and the soldiers had to double themselves up and stand on their hands and feet to make the arches.\n";
+
+        // Add choices when playing
+
+        std::cout << "The players all played at once without waiting for turns, quarrelling all the while, and fighting for the hedgehogs; \n"
+                     "and in a very short time the Queen was in a furious passion, and went stamping about, and shouting : ";
+
+        queen.talk("OFF WITH HER HEAD");
+        queen.talk("OFF WITH HIS HEAD");
+        queen.talk("OFF WITH THEIR HEAD");
+
+        // Add item check
+        std::cout << "You start to feel uneasy. Perhaps you best be leaving, the Queen is clearly in a rotten mood. \n"
+                     "While you are trying to sneak away, a familiar face returns... ";
+
+        cheshirecat.talk("How are you getting on?");
+        custom_character.talk("I don’t think they play at all fairly, and they all quarrel so dreadfully one can’t hear oneself speak—and they don’t seem to have any rules in particular; \n "
+                   "at least, if there are, nobody attends to them—and you’ve no idea how confusing it is all the things being alive; \n "
+                   "for instance, there’s the arch I’ve got to go through next walking about at the other end of the ground—and I should have croqueted the Queen’s hedgehog just now, only it ran away when it saw mine coming!");
+
+        cheshirecat.talk("How do you like the Queen?");
+
+        custom_character.talk("Not at all ; she’s so extremely—");
+        std::cout << "Just then you noticed that the Queen was close behind you, listening: so she went on :";
+        custom_character.talk("—likely to win, that it’s hardly worth while finishing the game.");
+
+        std::cout << "The Queen smiled and passed on.";
+
+        king.talk("Who are you talking to?");
+
+        custom_character.talk("It's a friend of mine—a Cheshire Cat, allow me to introduce it.");
+
+        king.talk("I don't like the look of it at all, however, it may kiss my hand if it likes.");
+
+        cheshirecat.talk("I'd rather not,");
+
+        king.talk("Don't be impertinent, and don’t look at me like that!");
+
+        custom_character.talk("A cat may look at a king, I've read that in some book, but I don't remember where.");
+
+        king.talk("Well, it must be removed, -- My dear! I wish you would have this cat removed!");
+
+        std::cout << "The Queen seemed to have only one way of settling all difficulties, great or small : ";
+        queen.talk("Off with his head!");
+
+        king.talk("I’ll fetch the executioner myself,");
+
+        std::cout << "He then hurried off.";
+
+        std::cout << "It might be worth going back and see how the game is going on, "
+                     "as you hear the Queen’s voice in the distance, screaming with passion. \n";
+
+        std::cout << "You had already heard her sentence three of the players to be executed for having missed their turns. \n"
+                     "When you arrive, you are uncertain if it is your turn or not so you just stand waiting, looking for your hedgehog. \n ";
+
+        std::cout << "Out of the corner of your eye, you see your hedgehog scurrying towards a large crowd. Without thinking you run after it only to realize \n"
+        "the large crowd is gathered around where the cheshire cat was. \n";
+
+        std::cout <<"There was a dispute going on between the executioner, the King, and the Queen, who were all talking at once, while all the rest were quite silent and looked very uncomfortable. \n"
+                    "The moment you show up, they stop and plead their cases to you.";
+
+        executioner.talk("You cannot cut off a head unless there was a body to cut it off from");
+
+        king.talk("Anything that had a head could be beheaded.");
+
+        queen.talk("If something isn't done about it in less than no time, I'll have everybody executed, all around.");
+
+        custom_character.talk("It belongs to the Duchess: you’d better ask her about it.");
+
+        std::cout << "The Queen turned to face the executioner :" ;
+
+        queen.talk("She’s in prison: fetch her here.");
+
+        std::cout << "And the executioner went off like an arrow. \n";
+
+        std::cout << "The Cat’s head began fading away the moment he was gone, all while throwing you a sly grin and wink. \n"
+                     "and, by the time he had come back with the Duchess, it had entirely disappeared;"
+                     "so the King and the executioner ran wildly up and down looking for it, while the rest of the party went back to the game. \n";
 
 
 
