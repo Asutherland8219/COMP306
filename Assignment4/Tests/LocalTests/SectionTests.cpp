835812de6496421032ec2639154b74e70d7c55b1
Asutherland8219 <60415557+Asutherland8219@users.noreply.github.com>
2024-03-22 20:59:15 -0300

    add some more chapter tests

--- Assignment4/Tests/LocalTests/SectionTests.cpp
+++ Assignment4/Tests/LocalTests/SectionTests.cpp
@@ -12,3 +12,20 @@
     Instead of running the function top to bottom, can run each section as desired.
 
 */
+
+#include "../../Character/Character.h"
+#include "../../Sections/Intro.h"
+#include "../../Sections/Welcome_to_wonderland.h"
+#include "../../Sections/Meeting_the_queen.h"
+#include "../../Sections/Tea_party.h"
+#include "../../Sections/Finale.h"
+
+Character TestIntro(Character custom_character) {
+    Character intro_char = Intro::startAliceInWonderland(custom_character);
+    return intro_char;
+};
+
+Character TestChapter1(Character intro_character) {
+    Character ch_1_character = Checkpoint1::Chapter1(intro_character);
+    return ch_1_character;
+}
